efd85084b7c06b35d4f3676632978bb01082ae99
hasikp1 <kphasitha1@gmail.com>
2021-01-11 11:18:34 +0530

    changed buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let img= document.querySelector('img');
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
-let btn2 = document.querySelector("#btn3");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
 
 btn1.addEventListener('click', ()=> {
     
